ed0dde6b83430db5a53f8b3d2c0b7b53e4080e5c
NathanBlue12 <110040051+NathanBlue12@users.noreply.github.com>
2022-09-11 13:20:34 +0800

    Updated

--- backend/AuthSys.py
+++ backend/AuthSys.py
@@ -1,37 +1,53 @@
-from datetime import date
-from datetime import time
-from database import database
 from pymongo import MongoClient
+from database import database
+from datetime import date
+from datetime import datetime
+
+# datetime object containing current date and time
+#Time
+now = datetime.now()
+#Date
+today = date.today()
 
 
 cluster = MongoClient("mongodb+srv://prefabdev:prefab9269@prefabcluster.fmgvmiy.mongodb.net/test")
+client = cluster
+
+mainDB = client.main 
+
+accounts_collection = mainDB.accounts 
+admins_collection = mainDB.admins 
+employees_collection = mainDB.employees
+log_records_collection = mainDB.log_records
+projects_collection = mainDB.projects
+tasks_collection = mainDB.tasks
+
 db = cluster["main"]
 collection = db["accounts"]
 
 
 
 
-
-def get_Account_LogIn_Details(account):
-
-
+# Checker
+def Authorization (username):
 
 #Find The Data inside the Collection
-    account = collection.find_one({"account_username": usr})
-    password = collection.find_one({"account_password": pwr})
+    usr = accounts_collection.find_one({"account_username": username})
+    pwr = accounts_collection.find_one({"account_password": password})
 
-    print(account)
+    #print(pwr)
+    
     
     try:
         #username
-        if account != None:
-            print("Found Username")
+        if usr != None:
+            #print("Found Username")
             try:
                 #password
-                if password != None:
-                            print("Found Password")
+                if pwr != None:
+                            #print("Found Password")
                             return True
-
+                            
             except:
                 print("Password not found")
                 return False
@@ -43,37 +59,48 @@ def get_Account_LogIn_Details(account):
         return False
 
 
+#Find ID
+def account_id(username):
+        found_id = accounts_collection.find_one({"account_username": username}, {"account_id": 1, "_id" : 0})
+        account_id = found_id['account_id']
+        return account_id
 
-usr = input("Username: ")
-pwr = input("Password: ")
 
 
-get_Account_LogIn_Details(usr)
+    # Textual month, day and year	
+date = today.strftime("%Y %B, %d")
+#print("d2 =", date)
+# Current Time
+time = now.strftime("%H:%M")
+#print("time =", time)	
 
-found_details = get_Account_LogIn_Details(usr)
 
+
+#Username input Remove Later
+username = input("Username: ")
+password = input("Password: ")
+
+
+#Verification Process
+Authorization(username)
+
+#Returns True or False
+Verified = Authorization(username)
+
+#Once Finish Verifying pass to get Account login details
 
 
 #Testing if True
-if found_details == True:
-    print("Yatta")
+if Verified == True:
+    #print("Yatta")
+    account_id(username= username)
+    database.get_Account_LogIn_Details(username= username)
+    #print(database.get_Account_LogIn_Details(username= username))
+    database.update_Time_In(account_id= account_id(username=username), date=date, time=time )
+    
 else:
     print("Ōno")    
 
-    
-
-
-
-
-
-
-
-
-#Insert Data Testing Phase Remove after deployment
-def Input():
-    post = {"account_username": "rian", "account_password": "goodbye", "employee_id": 22222, "account_id": 22222}
-    collection.insert_one(post)
-    print("Success")
 
 
 
@@ -87,13 +114,5 @@ def Input():
 
 
 
-
-
-
-
-
-
-    #db = client("Database")
-    #if usr == docTemplate_account and pwd == 'account_password '
 
 
