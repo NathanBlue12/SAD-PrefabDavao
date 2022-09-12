3e2174fbf583189f7ea39f60e106fbf11a8a137f
NathanBlue12 <110040051+NathanBlue12@users.noreply.github.com>
2022-09-12 16:16:40 +0800

    Update

--- backend/AuthSys.py
+++ backend/AuthSys.py
@@ -2,7 +2,6 @@ from database import database
 from datetime import date
 from datetime import datetime
 
-# datetime object containing current date and time
 #Time
 now = datetime.now()
 #Date
@@ -10,49 +9,72 @@ today = date.today()
 
 # Textual month, day and year	
 date = today.strftime("%Y-%m-%d")
-#print("d2 =", date)
 # Current Time
 time = now.strftime("%H:%M:%S")
-#print("time =", time)	
+
 
 class AuthSys():
     # Checker
+    global date, time
+    # Textual month, day and year	
+    date = today.strftime("%Y-%m-%d")
+    # Current Time
+    time = now.strftime("%H:%M:%S")
+
     def Authorization (user):
         
     #Find The Data inside the Collection
+        global userdetails
+        userdetails = database.get_Account_LogIn_Details(username=user)
+    
+
+
+        #userdetails 0 , 1 , 2 id , name , pass
         
+        #Returns True or False Verification Process
         databaseusr = userdetails[1] #Username Database
         databasepwr = userdetails[2] #password Database
         usr = user
         pwr = passw
+
         
+
         try:
-            #username
-            if usr == databaseusr:
-                print("Found Username")
+            if userdetails != "e0101db":
                 try:
-                    #password
-                    if pwr == databasepwr:
-                                print("Found Password")
-                                return True
-                                
+                    #username
+                    
+                    if usr == databaseusr:
+                        print("Found Username")
+                        try:
+                            #password
+                            if pwr == databasepwr:
+                                        print("Found Password")
+                                        return True
+                                        
+                        except:
+                            print("NOT FOUND")
+                            return False
+                    else:
+                        print("NOT FOUND")
+                        return False
                 except:
-                    print("Password not found")
                     return False
+            
             else:
-                print("Username not found")
+                print("NOT FOUND")
                 return False
         except:
-            print("No Data")
             return False
+
+    
     def LoggingIn(Verified):
         if Verified == True:
-            #print("Yatta")
             #userdetails 0 , 1 , 2 id , name , pass
             database.update_Time_In(account_id= userdetails[0], date=date, time=time )
             return True
-        else:
-            print("Ōno")    
+        
+
             
     def LoggingOut(Status):
         if Status == False:
@@ -60,19 +82,15 @@ class AuthSys():
         else:
             print("Still Logged on")
 
+    
+
+
+
 
 
 
 #Username input Remove Later
 user = input("Username: ")
 passw = input("Password: ")
-
-#userdetails 0 , 1 , 2 id , name , pass
-userdetails = database.get_Account_LogIn_Details(username=user)
-#Returns True or False Verification Process
-Verified = AuthSys.Authorization(user= user)
-
-AuthSys.Logging(Verified= Verified)
-
-
-
+AuthSys.Authorization(user=user)
+AuthSys.LoggingIn(Verified= AuthSys.Authorization(user= user))
