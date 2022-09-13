eae2ea5e7e0941860c53ebd8ebf12548a72cb96a
NathanBlue12 <110040051+NathanBlue12@users.noreply.github.com>
2022-09-13 09:45:33 +0800

    Update

--- backend/AuthSys.py
+++ backend/AuthSys.py
@@ -7,6 +7,7 @@ now = datetime.now()
 #Date
 today = date.today()
 
+
 class AuthSys():
     # Checker
     global date, time
@@ -14,22 +15,20 @@ class AuthSys():
     date = today.strftime("%Y-%m-%d")
     # Current Time
     time = now.strftime("%H:%M:%S")
-
-    def Authorization (user):
-        
-    #Find The Data inside the Collection
-        global userdetails
-        userdetails = database.get_Account_LogIn_Details(username=user)
     
 
-
+    def login (username, password):
+            #Find The Data inside the Collection
+        global userdetails
+        userdetails = database.get_Account_LogIn_Details(username=username)
+    
         #userdetails 0 , 1 , 2 id , name , pass
         
         #Returns True or False Verification Process
         databaseusr = userdetails[1] #Username Database
         databasepwr = userdetails[2] #password Database
-        usr = user
-        pwr = passw
+        usr = username
+        pwr = password
 
         
 
@@ -39,52 +38,43 @@ class AuthSys():
                     #username
                     
                     if usr == databaseusr:
-                        print("Found Username")
+                        #print("Found Username")
                         try:
                             #password
                             if pwr == databasepwr:
-                                        print("Found Password")
+                                        #print("Found Password")
                                         return True
                                         
                         except:
-                            print("NOT FOUND")
+                            
                             return False
                     else:
-                        print("NOT FOUND")
                         return False
                 except:
                     return False
             
             else:
-                print("NOT FOUND")
-                return False
+                return "NOT FOUND"
         except:
             return False
 
-    
-    def LoggingIn(Verified):
+
+    def Authorized (Verified,Status):
+
+    #Find The Data inside the Collection
         if Verified == True:
-            #userdetails 0 , 1 , 2 id , name , pass
+        #userdetails 0 , 1 , 2 id , name , pass
             database.update_Time_In(account_id= userdetails[0], date=date, time=time )
             return True
-        
-
-            
-    def LoggingOut(Status):
-        if Status == False:
-            database.update_Time_Out(account_id= userdetails[0], date=date, time=time )
-        else:
-            print("Still Logged on")
+        #elif Status == False:
+            #database.update_Time_Out(account_id= userdetails[0], date=date, time=time )
 
     
-
-
-
 
 
 
 #Username input Remove Later
-user = input("Username: ")
-passw = input("Password: ")
-AuthSys.Authorization(user=user)
-AuthSys.LoggingIn(Verified= AuthSys.Authorization(user= user))
+username = input("Username: ")
+password = input("Password: ")
+AuthSys.login(username=username, password=password)
+AuthSys.Authorized(Verified= AuthSys.login(username= username,password= password))
